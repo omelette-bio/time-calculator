c7cd5aee6cc4847a05b6012d0e101cd7765ece24
omelette-bio <omelettebio@proton.me>
2024-08-01 22:01:49 +0200

    added start of variable system

--- src/eval_error.rs
+++ src/eval_error.rs
@@ -1,15 +1,18 @@
 use std::fmt;
 
+#[derive(Debug)]
 pub enum EvalError {
     InvalidData,
-    NegativeResult
+    NegativeResult,
+    VariableNotFound(String)
 }
 
 impl fmt::Display for EvalError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             EvalError::InvalidData => write!(f, "Error, can't put negative values to duration."),
-            EvalError::NegativeResult => write!(f, "Error, the result of the substraction is negative.")
+            EvalError::NegativeResult => write!(f, "Error, the result of the substraction is negative."),
+            EvalError::VariableNotFound(v) => write!(f, "Error, the variable {} has no value.", v)
         }
     }  
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 mod time_data;
 mod eval_error;
+mod symbol_table;
 
 use time_data::TimeData;
 use eval_error::EvalError;
+use symbol_table::SymbolTable;
 
 fn main() {
-    let t_d1 = TimeData::new(0,48, 56,869);
-    let t_d2 = TimeData::new(0,48, 19,690);
-    let t_d3 = TimeData::new(0,49, 52,130);
+    let t_d1 = TimeData::new(0,45,32,461);
+    let t_d2 = TimeData::new(0,48,19,690);
+    let t_d3 = TimeData::new(0,43,39,632);
+    
+    let mut c = SymbolTable::new();
+    c.attribute_value("X".to_string(), t_d1);
+    
+    let res = c.read_variable("X".to_string());
+    let res2 = c.read_variable("C".to_string());
+
+    if let Err(m) = res { println!("{}", m);}
+    else { println!("{}", res.unwrap());}
+
+    if let Err(m) = res2 { println!("{}", m);}
+    else { println!("{}", res2.unwrap());}
+
     println!("{}", EvalError::InvalidData);
     println!("{}", EvalError::NegativeResult);
     println!("{}", t_d1);
     println!("{}", TimeData::from_ms(t_d1.to_ms()));
-    println!("{}", t_d1 - t_d2);
-    println!("{}", t_d3 - t_d1);
+    println!("{}", t_d2 - t_d1);
+    println!("{}", t_d2 - t_d3);
 }
 
 #[cfg(test)]
